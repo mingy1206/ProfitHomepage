554e35349a07c17554b615f15b2661f6ebaded8e
unknown <mingy837@gmail.com>
2024-03-19 09:14:41 +0900

    first commit

--- homepage/src/App.js
+++ homepage/src/App.js
@@ -1,25 +1,24 @@
 import './App.css';
-import Header from './components/header/Header';
-import Slide from './pages/main/Main';
 import Personal from './pages/personal/Personal';
 import SYJ from './pages/syj/syj_page';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import FullpageScroll from './pages/fullpage/FullPageScroll'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Header />
         <Routes>
-          <Route path="/" element={<Slide />} />
+          <Route path="/" element={<FullpageScroll />} />
           <Route path="personal" element={<Personal />} />
           <Route path="syj" element={<SYJ />} />
         </Routes>
-        <div style={{ marginTop: '100px' }}>
-          <Link to="/personal">Personal</Link><br />
-          <Link to="/syj">syj</Link>
-        </div>
+
       </BrowserRouter>
+
+
+
     </div>
   );
 }
